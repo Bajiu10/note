80557bea190c8b68d86de0ab53f2bab5dd9fec46
chengyao <987861463@qq.com>
2021-12-18 20:32:58 +0800

    添加log

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,11 @@
 
 namespace App\Exceptions;
 
+use Max\Foundation\App;
 use Max\Foundation\Exceptions\Handler as ExceptionHandler;
 use Max\Foundation\Exceptions\HttpException;
 use Max\Foundation\Http\Response;
+use Max\Log\LoggerFactory;
 use Max\Routing\Exceptions\RouteNotFoundException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -12,6 +14,17 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+    protected LoggerFactory $loggerFactory;
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        /** @var LoggerFactory $loggerFactory */
+        $this->loggerFactory = $app->make(LoggerFactory::class);
+    }
+
+
     /**
      * @param ServerRequestInterface $request
      * @param Throwable $throwable
@@ -25,6 +38,10 @@ class Handler extends ExceptionHandler
         if ($this->app->isDebug()) {
             return parent::render(...func_get_args());
         }
+        $this->loggerFactory->get('business')->info(
+            $throwable->getMessage(), [
+            'request' => $request->all(),
+        ]);
         return Response::make(view('mt/error', ['code' => $throwable->getCode(), 'message' => $throwable->getMessage()]), [], $throwable instanceof HttpException || $throwable instanceof RouteNotFoundException ? $throwable->getCode() : 500);
     }
 }
