3ecca1641e9e5d90582bdd7a42714e1e9f6abd95
chengyao <bigyao@139.com>
2021-12-25 12:53:00 +0800

    update

--- app/Http/Controllers/Api/Note.php
+++ app/Http/Controllers/Api/Note.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Dao\NoteDao;
 use App\Http\Controller;
-use App\Models\Notes;
-use Max\Http\UploadedFile;
+use Max\Http\Message\UploadedFile;
 use Max\Routing\Annotations\GetMapping;
 use Max\Routing\Annotations\PostMapping;
 
 /**
  * Class Note
+ *
  * @package App\Http\Controllers\Api
  */
 #[\Max\Routing\Annotations\Controller(prefix: 'api/notes', middleware: ['api'])]
@@ -18,6 +18,7 @@ class Note extends Controller
 {
     /**
      * @param NoteDao $noteDao
+     *
      * @return array
      */
     #[GetMapping(path: '/list')]
@@ -39,9 +40,9 @@ class Note extends Controller
     {
         /* @var UploadedFile $thumb */
         $thumb = $this->request->file('thumb');
-        $type = pathinfo($thumb->getClientFilename(), PATHINFO_EXTENSION);
-        $name = md5(microtime(true)) . '.' . $type;
-        $path = '/upload/thumb/' . date('Ymd/') . $name;
+        $type  = pathinfo($thumb->getClientFilename(), PATHINFO_EXTENSION);
+        $name  = md5(microtime(true)) . '.' . $type;
+        $path  = '/upload/thumb/' . date('Ymd/') . $name;
         if ($thumb instanceof UploadedFile) {
             $thumb->moveTo(public_path(ltrim($path, '/')));
             return ['status' => true, 'path' => $path];
@@ -59,15 +60,15 @@ class Note extends Controller
     public function uploadImages(): array
     {
         $image = $this->request->file('editormd-image-file');
-        $type = pathinfo($image->getClientFilename(), PATHINFO_EXTENSION);
-        $name = md5(microtime(true)) . '.' . $type;
-        $path = '/upload/images/' . date('Ymd/') . $name;
+        $type  = pathinfo($image->getClientFilename(), PATHINFO_EXTENSION);
+        $name  = md5(microtime(true)) . '.' . $type;
+        $path  = '/upload/images/' . date('Ymd/') . $name;
         if ($image instanceof UploadedFile) {
             $image->moveTo(public_path(ltrim($path, '/')));
             return [
                 'success' => 1,
                 'message' => '图片上传成功！',
-                'url' => $path
+                'url'     => $path
             ];
         }
         return [
