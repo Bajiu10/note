8b13d34e8bd2aef2913584fcdcf187f8ff1ee2ab
chengyao <987861463@qq.com>
2022-01-03 09:50:40 +0800

    更改样式

--- views/mt/about.blade.php
+++ views/mt/about.blade.php
@@ -17,32 +17,35 @@
         </p>
         <div id="step1" style="display: flex;justify-content: space-between;">
             <div class="card advantages" style="width:100%;margin-right: .5em">
-                <div class="tips2">特性</div>
+                <div class="tips">特性</div>
                 <div style="margin:.5em;color:#2d2626;line-height: 1.1em;text-align: left;font-size: 1.1em;padding:.5em;font-weight: bold">
                     <code>
                         一、&nbsp;组件和框架核心分离<br>
-                        二、&nbsp;基于Psr11的容器,支持接口注入 [注解注入PHP8.0]<br>
-                        三、&nbsp;支持门面，友好的IDE自动完成<br>
-                        四、&nbsp;基于Psr14的事件<br>
-                        五、&nbsp;DB类支持mysql、psql等[可扩展]<br>
-                        六、&nbsp;基于Psr15的中间件 [注解定义PHP8.0]<br>
-                        七、&nbsp;基于Psr16的缓存[支持File,Memcached,Redis]<br>
-                        八、&nbsp;跨域功能支持，支持全局跨域功能<br>
-                        九、&nbsp;支持路由功能 [注解定义PHP8.0]<br>
-                        十、&nbsp;其他：视图、命令行、验证器、Redis等<br>
+                        二、&nbsp;基于 Psr7 的 HTTP-Message<br>
+                        三、&nbsp;基于 Psr11 的容器，支持接口注入[支持注解 PHP8.0]<br>
+                        四、&nbsp;友好的 IDE 自动完成<br>
+                        五、&nbsp;支持门面<br>
+                        六、&nbsp;方便的数据库操作方法，支持 MySQL、PostgreSQL 等[可扩展]<br>
+                        七、&nbsp;基于 Psr14 的事件[支持注解 PHP8.0]<br>
+                        八、&nbsp;基于 Psr15 的中间件[支持注解 PHP8.0]<br>
+                        九、&nbsp;支持路由功能[支持注解 PHP8.0]<br>
+                        十、&nbsp;基于 Psr16 的缓存组件,支持 File,Memcached,Redis,APC[可扩展]<br>
+                        十、&nbsp;支持 Blade 的可扩展驱<br>
+                        十、&nbsp;简单易用的命令行操作<br>
+                        十、&nbsp;命令行，验证器，trace<br>
                     </code>
                 </div>
             </div>
             <div style="width:100%" class="card">
-                <div class="tips2">安装要求</div>
+                <div class="tips">安装要求</div>
                 <div style="margin:.5em;color:#2d2626;line-height: 1.1em;text-align: left;font-size: 1.1em;padding:.5em;font-weight: bold">
                     <code>PHP >= 7.4 √</code>
                 </div>
-                <div class="tips2">使用Composer安装</div>
+                <div class="tips">使用Composer安装</div>
                 <div style="margin:.5em;color:#2d2626;line-height: 1.1em;text-align: left;font-size: 1.1em;padding:.5em;font-weight: bold">
-                    <code>composer create-project --prefer-dist max/max:dev-master .</code>
+                    <code>composer create-project --prefer-dist max/max .</code>
                 </div>
-                <div class="tips2">
+                <div class="tips">
                     运行框架
                 </div>
                 <div style="margin:.5em;color:#2d2626;line-height: 1.1em;text-align: left;font-size: 1.1em;padding:.5em;font-weight: bold">
